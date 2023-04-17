470f212c3a462724e812ebe4bbada2c9e2de056b
leslie <lesmwendwa@gmail.com>
2023-04-17 16:33:02 +0300

    styling

--- ecommerce/store/utils.py
+++ ecommerce/store/utils.py
@@ -3,39 +3,42 @@ from . models import *
 
 
 def cookieCart(request):
+    #Create empty cart for now for non-logged in user
     try:
         cart = json.loads(request.COOKIES['cart'])
     except:
         cart = {}
     print('Cart:', cart)
+
     items = []
     order = {"get_cart_total": 0, "get_cart_items": 0, 'shipping':False}
     cartItems = order['get_cart_items']
 
     for i in cart:
         try:
-            cartItems += cart[i]["quantity"]
+            if(cart[i]['quantity']>0):
+                cartItems += cart[i]["quantity"]
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
 
-            item = {
-                'product':{
-                    'id':product.id,
-                    'name':product.name,
-                    'price':product.price,
-                    'imageURL':product.imageURL
-                    },
-                'quantity':cart[i]['quantity'],
-                'get_total':total,
-                }
-            items.append(item)
+                item = {
+                    'product':{
+                        'id':product.id,
+                        'name':product.name,
+                        'price':product.price,
+                        'imageURL':product.imageURL
+                        },
+                    'quantity':cart[i]['quantity'],
+                    'get_total':total,
+                    }
+                items.append(item)
 
-            if product.digital == False:
-                order['shipping'] = True
+                if product.digital == False:
+                    order['shipping'] = True
         except:
             pass
     return{'cartItems':cartItems, 'order':order, 'items':items}
@@ -75,11 +78,11 @@ def guestOrder(request, data):
     )
 
     for item in items:
-        product = Product.objects.get(id=item['product']['id'])
+        product = Product.objects.get(id=item['id'])
 
         orderItem = OrderItem.objects.create(
             product=product,
             order=order,
-            quantity=item['quantity']
+            quantity=(item['quantity'] if item['quantity']>0 else -1*item['quantity']), # negative quantity = freebies
         )
     return customer, order
